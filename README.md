689ad5c6a257e339c96ca512b8745acd0775442b
priti <priti.biyani6@gmail.com>
2016-01-31 19:24:39 +0530

    Implementing quanity for length

---
+++ README.md
@@ -0,0 +1,2 @@
+1. 12 Inch = 1 feet
+2. 100 CM = 1 meter
---
+++ src/test/LengthCompare.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class LengthCompare {
+    @Test
+    public void shouldReturnTrueWhenComparing12Inchand1Foot(){
+        Length tweleveInch = new Length(12, QUANTITY.INCH);
+    }
+}
